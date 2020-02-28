c8fffacb8ce2440b823643e37b0455cce5fcbfaa
Pro-Use <rob@prou.se>
2020-02-28 12:34:29 +0000

    first commit

---
+++ entry.php
@@ -0,0 +1,87 @@
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <style>.pac-logo:after{display:none;}</style>
+</head>
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+include 'oauth.php';
+if ( isset( $_POST['name'] ) && isset( $_POST['email'] ) ) {
+    $name = $_POST['name'];
+    $date = $_POST['date'];
+    $geo = $_POST['geo'];
+    $email = $_POST['email'];
+    $payload = json_encode($_POST);
+    
+    $token = getToken();
+    
+    // Prepare new cURL resource
+    $ch = curl_init('http://localhost:44444/arrivals');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+     
+    // Set HTTP Header for POST request 
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($payload),
+        "authorization: Bearer " . $token
+        )
+    );
+     
+    // Submit the POST request
+    $result = curl_exec($ch);
+     
+    // Close cURL session handle
+    curl_close($ch);
+    echo($result);
+}
+?>
+
+<body>
+<form  method="post">
+  Date:<br>
+  <input type="text" name="date" value="1 Jan"><br>
+  Last name:<br>
+  <input type="text" name="name" value="Rob"><br>
+  <br>
+  <div id="locationField">
+    <input id="autocomplete"
+           placeholder="Enter your address"
+           type="text"/>
+  </div>
+  Geo:<br>
+  <input id="latlng" type="text" name="geo" value="" disabled="true"><br>
+  Email:<br>
+  <input type="text" name="email" value=""><br>
+  <br>
+  <input type="submit" value="Submit">
+</form>
+    
+<script>
+    var placeSearch, autocomplete;
+    
+    function initAutocomplete() {
+      autocomplete = new google.maps.places.Autocomplete(
+              document.getElementById('autocomplete'), {types: ['geocode']});
+      autocomplete.setFields(['geometry']);
+      autocomplete.addListener('place_changed', fillInAddress);
+    }  
+
+    
+    function fillInAddress() {
+        // Get the place details from the autocomplete object.
+        var place = autocomplete.getPlace();
+        var lat = place.geometry.location.lat();
+        var lng = place.geometry.location.lng();
+        document.getElementById("latlng").value = lat + "," + lng;
+        document.getElementById("latlng").disabled = false;
+      }
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDz69AZONqQhR3cBzeYiAqiaDaoVRHaR5A&libraries=places&callback=initAutocomplete"
+   async defer></script>
+</body>
---
+++ index.php
@@ -0,0 +1,63 @@
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<script src='https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js'></script>
+
+</head>
+<body>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include 'oauth.php';
+$token = getToken();
+
+
+$curl = curl_init();
+curl_setopt_array($curl, [
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'http://localhost:44444/arrivals/webBoard',
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => array(
+      "authorization: Bearer " . $token),
+]);
+$results = json_decode(curl_exec($curl), true);
+curl_close($curl);
+
+$board_line = 1;
+foreach ($results as $result) {
+    echo ('<div id="board-line-' .$board_line. '">
+        <span id="board-date-' .$board_line. '">' .$result["date"]. '</span>
+        <span id="board-name-' .$board_line. '">' . $result["name"]. '</span>
+    </div>');
+    $board_line += 1;
+} 
+
+?>
+</body>
+<script>
+  var socket = io.connect('http://localhost:44444');
+
+  socket.on('connect', function(data) {
+    	socket.emit('join', 'Hello World from client');
+    	console.log('connected to socket');
+  }); 
+ 
+  socket.on('new_names', function(data) {
+	console.log(data);
+	var board_line = 1;
+	data.forEach(function(item) {
+	  document.getElementById('board-date-'+board_line).innerHTML = item["date"];
+	  document.getElementById('board-name-'+board_line).innerHTML = item["name"];
+	  board_line += 1;
+	});
+	
+  });
+
+
+</script>
+</html>
