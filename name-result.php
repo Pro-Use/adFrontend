915427c48a9edda4d2710241c3ff6a3d36e63db0
pro-use <robertprouse@gmail.com>
2020-03-02 16:10:40 +0000

    check names against census data

--- name-result.php
+++ name-result.php
@@ -32,6 +32,7 @@
     if ( isset( $_POST['name'] )) {
         $name = $_POST['name'];
         $q_name = str_replace(" ", "%20", $name);
+        $name_count = substr_count($q_name, "%20");
         $URL = "https://globalname.melissadata.net/V3/WEB/GlobalName/doGlobalName?";
         $query = "t=1&id=" . $MELISSA_KEY . "&opt=''&comp=''&full=" . $q_name . "&format=json";   
         $curl = curl_init();
@@ -43,6 +44,7 @@
         $results = $result["Records"][0]["Results"];
         $codes = explode(",", $results);
         $errors = 0;
+        $census_matches = 0;
         echo("<h1>Name: ".$name."</h1><br><br><h1>Results</h1>");
         foreach ($codes as $code) {
             if (substr($code, 0,2) == "NE") {
@@ -50,7 +52,14 @@
                 $errors += 1;
             } elseif (substr($code, 0,2) == "NS") {
                 echo("<div>SUCCESS: " . $CODES[$code] . "</div>");
+                if (intval(substr($code, 3,4)) >= 5 || intval(substr($code, 3,4)) <= 8) {
+                    $census_matches += 1;
+                }
             }
+        }
+        if ($census_matches < $name_count) {
+            echo("<div>ERROR: Not all names match census data</div>");
+            $errors += 1;
         }
         if ($errors > 0) {
             echo("<br><div><h2>***This result would be moderated***</h2></div>");
