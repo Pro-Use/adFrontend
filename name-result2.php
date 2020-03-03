fbc1e702d6e4e986b339da08bdae05b2fe648ead
pro-use <robertprouse@gmail.com>
2020-03-03 14:03:32 +0000

    print nameapi results

--- name-result2.php
+++ name-result2.php
@@ -9,36 +9,42 @@
     error_reporting(E_ALL);
     
     require_once('org/nameapi/client/services/ServiceFactory.php');
-    
+
     use org\nameapi\ontology\input\context\Context;
     use org\nameapi\ontology\input\context\Priority;
     use org\nameapi\client\services\ServiceFactory;
-    $context = Context::builder()
-        ->place('US')
-//        ->priority(Priority::REALTIME)
-        ->build();
-  
-    $serviceFactory = new ServiceFactory('e613e25a6980a345cd9b973e10dd6b42-user1', $context);
-    
-    $ping = $serviceFactory->systemServices()->ping();
-    $pong = $ping->ping();
-    
     use org\nameapi\ontology\input\entities\person\NaturalInputPerson;
     use org\nameapi\ontology\input\entities\person\name\InputPersonName;
-
-    $inputPerson = NaturalInputPerson::builder()
-            ->name(InputPersonName::westernBuilder()
-                    ->fullname("John F. Kennedy")
-                    ->build())
-            ->build();
     
-    $personNameParser = $serviceFactory->parserServices()->personNameParser();
-    $parseResult = $personNameParser->parse($inputPerson);
-    $bestMatch = $parseResult->getBestMatch();
-    var_dump($bestMatch->getLikeliness());
-
-if ( isset( $_POST['name'] )) {
+    if ( isset( $_POST['name'] )) {
         $name = $_POST['name'];
+
+        $context = Context::builder()
+            ->place('US')
+    //        ->priority(Priority::REALTIME)
+            ->build();
+
+        $serviceFactory = new ServiceFactory('e613e25a6980a345cd9b973e10dd6b42-user1', $context);
+
+        $ping = $serviceFactory->systemServices()->ping();
+        $pong = $ping->ping();
+
+
+        $inputPerson = NaturalInputPerson::builder()
+                ->name(InputPersonName::westernBuilder()
+                        ->fullname("John F. Kennedy")
+                        ->build())
+                ->build();
+
+        $personNameParser = $serviceFactory->parserServices()->personNameParser();
+        $parseResult = $personNameParser->parse($inputPerson);
+        $bestMatch = $parseResult->getBestMatch();
+        $likeliness = $bestMatch->getLikeliness() * 100;
+        $confidence = $bestMatch->getConfidence() * 100;
+
+        echo("<h1>Name: ".$name."</h1><br><br><h1>Results</h1>");
+        echo("<div>Likeliness name is real: " . $likeliness . "% </div>");
+        echo("<div>confidence: " . $confidence . "% </div>");
  
     }
     ?>
