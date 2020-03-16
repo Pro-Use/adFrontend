cd6489769ef7add041b6883cc9f98ed47dcab35a
pro-use <robertprouse@gmail.com>
2020-03-16 17:35:38 +0000

    Add departures to map gen

--- get_map.php
+++ get_map.php
@@ -22,6 +22,21 @@ curl_setopt_array($curl, [
 $arr_geo = json_decode(curl_exec($curl), true);
 curl_close($curl);
 
+$curl = curl_init();
+curl_setopt_array($curl, [
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'http://localhost:44444/arrivals/map',
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => array(
+        "authorization: Bearer " . $token),
+]);
+$dep_geo = json_decode(curl_exec($curl), true);
+curl_close($curl);
+
 // Start XML file, create parent node
 $dom = new DOMDocument("1.0");
 $node = $dom->createElement("markers");
@@ -30,6 +45,17 @@ $parnode = $dom->appendChild($node);
 header("Content-type: text/xml");
 
 foreach ($arr_geo as $row) {
+  $node = $dom->createElement("marker");
+  $newnode = $parnode->appendChild($node);
+  $newnode->setAttribute("name", $row['name']);
+  $newnode->setAttribute("date", $row['date']);
+  $latlng = explode(',',  $row['geo']);
+  $newnode->setAttribute("lat", $latlng[0]);
+  $newnode->setAttribute("lng", $latlng[1]);
+  $newnode->setAttribute("type", 'arrival');
+}
+
+foreach ($dep_geo as $row) {
   $node = $dom->createElement("marker");
   $newnode = $parnode->appendChild($node);
   $newnode->setAttribute("name", $row['name']);
