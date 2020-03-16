2c9dce1994055d12a23e94089d587b8edeae9ea3
pro-use <robertprouse@gmail.com>
2020-03-16 17:29:25 +0000

    Add map page

--- map.php
+++ map.php
@@ -36,7 +36,7 @@
         function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
           center: new google.maps.LatLng(0, 0),
-          zoom: 4
+          zoom: 3
         });
         var infoWindow = new google.maps.InfoWindow;
 
