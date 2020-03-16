162509125fc6924e8a9b746849265b5069a88690
pro-use <robertprouse@gmail.com>
2020-03-16 14:44:59 +0000

    add departures manual moderation page

--- moderation.php
+++ moderation.php
@@ -10,7 +10,7 @@ error_reporting(E_ALL);
 include 'oauth.php';
 $token = getToken();
 
-
+# Arrivals
 $curl = curl_init();
 curl_setopt_array($curl, [
     CURLOPT_RETURNTRANSFER => 1,
@@ -27,6 +27,41 @@ $results = json_decode(curl_exec($curl), true);
 curl_close($curl);
 
 $board_line = 1;
+echo("<h1>Arrivals:</h1>");
+foreach ($results as $result) { ?>
+    <div id="board-line-<?php echo($board_line);?>">
+        <span><?php echo($result["date"]);?></span>
+        <span><?php echo($result["name"]);?></span>
+        <form method="post" action="update.php">
+            <input type="hidden" value="<?php echo($result["ID"]);?>" name="ID" id="ID">
+            <input type="radio" id="accept" name="update" value="accept">
+            <label for="accept">Accept</label>
+            <input type="radio" id="delete" name="update" value="delete">
+            <label for="delete">Delete</label>
+            <input type="submit" value="Update">
+        </form>
+    </div>
+   <?php $board_line += 1;
+} 
+
+# Arrivals
+$curl = curl_init();
+curl_setopt_array($curl, [
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'http://localhost:44444/departuress/moderation',
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => array(
+      "authorization: Bearer " . $token),
+]);
+$results = json_decode(curl_exec($curl), true);
+curl_close($curl);
+
+$board_line = 1;
+echo("<h1>Arrivals:</h1>");
 foreach ($results as $result) { ?>
     <div id="board-line-<?php echo($board_line);?>">
         <span><?php echo($result["date"]);?></span>
