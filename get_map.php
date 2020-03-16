0276b89b7603b6ae10051963f4a3929d9d0122f2
pro-use <robertprouse@gmail.com>
2020-03-16 16:49:56 +0000

    Add map xml gen

--- get_map.php
+++ get_map.php
@@ -22,25 +22,25 @@ curl_setopt_array($curl, [
 $arr_geo = json_decode(curl_exec($curl), true);
 curl_close($curl);
 
-// Start XML file, create parent node
-$dom = new DOMDocument("1.0");
-$node = $dom->createElement("markers");
-$parnode = $dom->appendChild($node);
-
-header("Content-type: text/xml");
-
-foreach ($arr_geo as $row) {
-  $node = $dom->createElement("marker");
-  $newnode = $parnode->appendChild($node);
-
-  $newnode->setAttribute("id", $row['ID']);
-  $newnode->setAttribute("name", $row['name']);
-  $newnode->setAttribute("date", $row['date']);
-  $latlng = explode(',',  $row['geo']);
-  $newnode->setAttribute("lat", $latlng[0]);
-  $newnode->setAttribute("lng", $latlng[1]);
-  $newnode->setAttribute("type", 'arrival');
-}
-
-echo $dom->saveXML();
+//// Start XML file, create parent node
+//$dom = new DOMDocument("1.0");
+//$node = $dom->createElement("markers");
+//$parnode = $dom->appendChild($node);
+//
+//header("Content-type: text/xml");
+//
+//foreach ($arr_geo as $row) {
+//  $node = $dom->createElement("marker");
+//  $newnode = $parnode->appendChild($node);
+//
+//  $newnode->setAttribute("id", $row['ID']);
+//  $newnode->setAttribute("name", $row['name']);
+//  $newnode->setAttribute("date", $row['date']);
+//  $latlng = explode(',',  $row['geo']);
+//  $newnode->setAttribute("lat", $latlng[0]);
+//  $newnode->setAttribute("lng", $latlng[1]);
+//  $newnode->setAttribute("type", 'arrival');
+//}
+//
+//echo $dom->saveXML();
 var_dump($arr_geo);
